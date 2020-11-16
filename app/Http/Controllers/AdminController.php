5e79fb6f12af086a5be77ac3c2dbc77c22edce26
nhintt <nhintt2411@gmail.com>
2020-11-17 00:45:09 +0700

    Thống kê doanh số (163 - 166)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use Illuminate\Http\Request;
 use DB;
 use Session;
@@ -11,123 +12,257 @@ use App\Login;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Redirect;
 use Validator;
-use App\Rules\Captcha; 
+use App\Rules\Captcha;
+use App\Statistic;
+use App\Visitors;
+use App\Order;
+use App\Product;
+use Carbon\Carbon;
+use Doctrine\DBAL\Schema\Visitor\Visitor;
+use ReCaptcha\RequestMethod\Post;
+
 class AdminController extends Controller
 {
-    public function login_google(){
+    public function login_google()
+    {
         return Socialite::driver('google')->redirect();
     }
-    public function callback_google(){
-            $users = Socialite::driver('google')->stateless()->user(); 
-            // // return $users->id;
-            // return $users->name;
-            // return $users->email;
-            $authUser = $this->findOrCreateUser($users,'google');
-            $account_name = Login::where('admin_id',$authUser->user)->first();
-            Session::put('admin_name',$account_name->admin_name);
-            Session::put('admin_id',$account_name->admin_id);
-            return redirect('/dashboard')->with('message', 'Đăng nhập Admin thành công');  
+    public function callback_google()
+    {
+        $users = Socialite::driver('google')->stateless()->user();
+        // // return $users->id;
+        // return $users->name;
+        // return $users->email;
+        $authUser = $this->findOrCreateUser($users, 'google');
+        $account_name = Login::where('admin_id', $authUser->user)->first();
+        Session::put('admin_name', $account_name->admin_name);
+        Session::put('admin_id', $account_name->admin_id);
+        return redirect('/dashboard')->with('message', 'Đăng nhập Admin thành công');
     }
-    public function findOrCreateUser($users, $provider){
-            $authUser = Social::where('provider_user_id', $users->id)->first();
-            if($authUser){
+    public function findOrCreateUser($users, $provider)
+    {
+        $authUser = Social::where('provider_user_id', $users->id)->first();
+        if ($authUser) {
 
-                return $authUser;
-            }
-          
-            $hieu = new Social([
-                'provider_user_id' => $users->id,
-                'provider' => strtoupper($provider)
+            return $authUser;
+        }
+
+        $hieu = new Social([
+            'provider_user_id' => $users->id,
+            'provider' => strtoupper($provider)
+        ]);
+
+        $orang = Login::where('admin_email', $users->email)->first();
+
+        if (!$orang) {
+            $orang = Login::create([
+                'admin_name' => $users->name,
+                'admin_email' => $users->email,
+                'admin_password' => '',
+                'admin_phone' => '',
+                'admin_status' => 1
+
             ]);
+        }
 
-            $orang = Login::where('admin_email',$users->email)->first();
+        $hieu->login()->associate($orang);
 
-                if(!$orang){
-                    $orang = Login::create([
-                        'admin_name' => $users->name,
-                        'admin_email' => $users->email,
-                        'admin_password' => '',
-                        'admin_phone' => '',
-                        'admin_status' => 1
-                        
-                    ]);
-                }
+        $hieu->save();
 
-            $hieu->login()->associate($orang);
-                
-            $hieu->save();
+        $account_name = Login::where('admin_id', $hieu->user)->first();
+        Session::put('admin_name', $account_name->admin_name);
+        Session::put('admin_id', $account_name->admin_id);
 
-            $account_name = Login::where('admin_id',$hieu->user)->first();
-            Session::put('admin_name',$account_name->admin_name);
-            Session::put('admin_id',$account_name->admin_id); 
-          
-            return redirect('/dashboard')->with('message', 'Đăng nhập Admin thành công');
-
-
+        return redirect('/dashboard')->with('message', 'Đăng nhập Admin thành công');
     }
 
 
-    public function login_facebook(){
+    public function login_facebook()
+    {
         return Socialite::driver('facebook')->redirect();
     }
 
-    public function callback_facebook(){
+    public function callback_facebook()
+    {
         $provider = Socialite::driver('facebook')->user();
-        $account = Social::where('provider','facebook')->where('provider_user_id',$provider->getId())->first();
-        if($account){
-            //login in vao trang quan tri  
-            $account_name = Login::where('admin_id',$account->user)->first();
-            Session::put('admin_name',$account_name->admin_name);
-            Session::put('admin_id',$account_name->admin_id);
+        $account = Social::where('provider', 'facebook')->where('provider_user_id', $provider->getId())->first();
+        if ($account) {
+            //login in vao trang quan tri
+            $account_name = Login::where('admin_id', $account->user)->first();
+            Session::put('admin_name', $account_name->admin_name);
+            Session::put('admin_id', $account_name->admin_id);
             return redirect('/dashboard')->with('message', 'Đăng nhập Admin thành công');
-        }else{
+        } else {
 
             $hieu = new Social([
                 'provider_user_id' => $provider->getId(),
                 'provider' => 'facebook'
             ]);
 
-            $orang = Login::where('admin_email',$provider->getEmail())->first();
+            $orang = Login::where('admin_email', $provider->getEmail())->first();
 
-            if(!$orang){
+            if (!$orang) {
                 $orang = Login::create([
                     'admin_name' => $provider->getName(),
                     'admin_email' => $provider->getEmail(),
                     'admin_password' => '',
                     'admin_phone' => ''
-                    
+
                 ]);
             }
             $hieu->login()->associate($orang);
             $hieu->save();
 
-            $account_name = Login::where('admin_id',$account->user)->first();
-            Session::put('admin_name',$account_name->admin_name);
-            Session::put('admin_id',$account_name->admin_id);
+            $account_name = Login::where('admin_id', $account->user)->first();
+            Session::put('admin_name', $account_name->admin_name);
+            Session::put('admin_id', $account_name->admin_id);
             return redirect('/dashboard')->with('message', 'Đăng nhập Admin thành công');
-        } 
+        }
     }
 
-    public function AuthLogin(){
+    public function AuthLogin()
+    {
         $admin_id = Session::get('admin_id');
-        if($admin_id){
+        if ($admin_id) {
             return Redirect::to('dashboard');
-        }else{
+        } else {
             return Redirect::to('admin')->send();
         }
     }
 
-    public function index(){
-    	return view('admin_login');
+    public function index()
+    {
+        return view('admin_login');
     }
-    public function show_dashboard(){
+    public function show_dashboard(Request $request)
+    {
         $this->AuthLogin();
-    	return view('admin.dashboard');
+        // $user_ip_address = '150.13.005.189';
+        $user_ip_address = $request->ip();
+        $early_last_month = Carbon::now('Asia/Ho_Chi_Minh')->subMonth()->startOfMonth()->toDateString();
+        $end_of_last_month = Carbon::now('Asia/Ho_Chi_Minh')->subMonth()->endOfMonth()->toDateString();
+        $early_this_month = Carbon::now('Asia/Ho_Chi_Minh')->startOfMonth()->toDateString();
+        $oneyears = Carbon::now('Asia/Ho_Chi_Minh')->subDays(365)->toDateString();
+        $now = Carbon::now('Asia/Ho_Chi_Minh')->toDateString();
+
+        //total last month
+        $visitor_of_lastmonth = Visitors::whereBetween('date_visitor', [$early_last_month, $end_of_last_month])->get();
+        $visitor_last_month_count = $visitor_of_lastmonth->count();
+
+        //total this month
+        $visitor_of_thismonth = Visitors::whereBetween('date_visitor', [$early_this_month, $now])->get();
+        $visitor_this_month_count = $visitor_of_thismonth->count();
+
+        //total one year
+        $visitor_of_year = Visitors::whereBetween('date_visitor', [$oneyears, $now])->get();
+        $visitor_year_count = $visitor_of_year->count();
+
+        //current online
+        $visitors_current = Visitors::where('ip_address', $user_ip_address)->get();
+        $visitor_count = $visitors_current->count();
+
+        if ($visitor_count < 1) {
+            $visitor = new Visitors();
+            $visitor->ip_address = $user_ip_address;
+            $visitor->date_visitor = Carbon::now('Asia/Ho_Chi_Minh')->toDateString();
+            $visitor->save();
+        }
+        //total visitors
+        $visitors = Visitors::all();
+        $visitors_total = $visitors->count();
+        //total
+        // $product = Product::all()->count();
+        // $product_views = Product::orderBy('product_views', 'DESC')->take(20)->get();
+        // $post = Post::all()->count();
+        // $post_views = Post::orderBy('post_views', 'DESC')->take(20)->get();
+        // $order = Order::all()->count();
+        // $video = Video::all()->count();
+        // $customer = Customer::all()->count();
+
+
+        return view('admin.dashboard')->with(compact('visitor_count', 'visitors_total', 'visitor_last_month_count', 'visitor_this_month_count', 'visitor_year_count'));
     }
-    public function dashboard(Request $request){
+    public function days_order()
+    {
+        $sub30days = Carbon::now('Asia/Ho_Chi_Minh')->subDays(30)->toDateString();
+
+        $now = Carbon::now('Asia/Ho_Chi_Minh')->toDateString();
+        $get = Statistic::whereBetween('order_date', [$sub30days, $now])->orderBy('order_date', 'ASC')->get();
+
+        foreach ($get as $key => $val) {
+            $chart_data[] = array(
+                'period' => $val->order_date,
+                'order' => $val->total_order,
+                'sales' => $val->sales,
+                'profit' => $val->profit,
+                'quantity' => $val->quantity
+            );
+        }
+        echo $data = json_encode($chart_data);
+    }
+    public function dashboard_filter(Request $request)
+    {
+        $data = $request->all();
+
+        $dauthangnay = Carbon::now('Asia/Ho_Chi_Minh')->startOfMonth()->toDateString();
+        $dau_thangtruoc = Carbon::now('Asia/Ho_Chi_Minh')->subMonth()->startOfMonth()->toDateString();
+        $cuoi_thangtruoc = Carbon::now('Asia/Ho_Chi_Minh')->subMonth()->endOfMonth()->toDateString();
+
+        $sub7days = Carbon::now('Asia/Ho_Chi_Minh')->subDays(7)->toDateString();
+        $sub365days = Carbon::now('Asia/Ho_Chi_Minh')->subDays(365)->toDateString();
+
+        $now = Carbon::now('Asia/Ho_Chi_Minh')->toDateString();
+        if ($data['dashboard_value'] == '7ngay') {
+            $get = Statistic::whereBetween('order_date', [$sub7days, $now])->orderBy('order_date', 'ASC')->get();
+        } elseif ($data['dashboard_value'] == 'thangtruoc') {
+            $get = Statistic::whereBetween('order_date', [$dau_thangtruoc, $cuoi_thangtruoc])->orderBy('order_date', 'ASC')->get();
+        } elseif ($data['dashboard_value'] == 'thangnay') {
+            $get = Statistic::whereBetween('order_date', [$dauthangnay, $now])->orderBy('order_date', 'ASC')->get();
+        } else {
+            $get = Statistic::whereBetween('order_date', [$sub365days, $now])->orderBy('order_date', 'ASC')->get();
+        }
+
+        foreach ($get as $key => $val) {
+            $chart_data[] = array(
+                'period' => $val->order_date,
+                'order' => $val->total_order,
+                'sales' => $val->sales,
+                'profit' => $val->profit,
+                'quantity' => $val->quantity
+            );
+        }
+        echo $data = json_encode($chart_data);
+    }
+    public function filter_by_date(Request $request)
+    {
+        $data = $request->all();
+
+        $from_date = $data['from_date'];
+        $to_date = $data['to_date'];
+
+        $get = Statistic::whereBetween('order_date', [$from_date, $to_date])->orderBy('order_date', 'ASC')->get();
+
+        foreach ($get as $key => $val) {
+            $chart_data[] = array(
+                'period' => $val->order_date,
+                'order' => $val->total_order,
+                'sales' => $val->profit,
+                'quantity' => $val->quantity
+            );
+        }
+        echo $data = json_encode($chart_data);
+    }
+    public function order_date(Request $request)
+    {
+        $order_date = $_GET['date'];
+        $order = Order::where('order_date', $order_date)->orderBy('created_at', 'DESC')->get();
+        return view('admin.order_date')->with(compact('order'));
+    }
+    public function dashboard(Request $request)
+    {
         //$data = $request->all();
         $data = $request->validate([
-            //validation laravel 
+            //validation laravel
             'admin_email' => 'required',
             'admin_password' => 'required',
             'g-recaptcha-response' => new Captcha(),    //dòng kiểm tra Captcha
@@ -135,25 +270,24 @@ class AdminController extends Controller
 
         $admin_email = $data['admin_email'];
         $admin_password = md5($data['admin_password']);
-        $login = Login::where('admin_email',$admin_email)->where('admin_password',$admin_password)->first();
-        if($login){
+        $login = Login::where('admin_email', $admin_email)->where('admin_password', $admin_password)->first();
+        if ($login) {
             $login_count = $login->count();
-            if($login_count>0){
-                Session::put('admin_name',$login->admin_name);
-                Session::put('admin_id',$login->admin_id);
+            if ($login_count > 0) {
+                Session::put('admin_name', $login->admin_name);
+                Session::put('admin_id', $login->admin_id);
                 return Redirect::to('/dashboard');
             }
-        }else{
-                Session::put('message','Mật khẩu hoặc tài khoản bị sai.Làm ơn nhập lại');
-                return Redirect::to('/admin');
+        } else {
+            Session::put('message', 'Mật khẩu hoặc tài khoản bị sai.Làm ơn nhập lại');
+            return Redirect::to('/admin');
         }
-       
-
     }
-    public function logout(){
+    public function logout()
+    {
         $this->AuthLogin();
-        Session::put('admin_name',null);
-        Session::put('admin_id',null);
+        Session::put('admin_name', null);
+        Session::put('admin_id', null);
         return Redirect::to('/admin');
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,110 @@
 @extends('admin_layout')
 @section('admin_content')
-<h3><center>Chào mừng bạn đến với đồ án hướng dẫn website của anh Hiếu nhé</center></h3>
+    <div class="container-fluid">
+        <style type="text/css">
+            p.title-thongke {
+                text-align: center;
+                font-size: 20px;
+                font-weight: bold;
+            }
+
+        </style>
+
+        <div class="row">
+            <p class="title-thongke">Thống kê đơn hàng doanh số</p>
+            <form autocomplete="off">
+                @csrf
+                <div class="col-md-2">
+                    <p>Từ ngày: <input type="text" id="datepicker" class="form-control"></p>
+                    <input type="button" id="btn-dashboard-filter" class="btn btn-primary btn-sm" value="Lọc kết quả">
+                </div>
+                <div class="col-md-2">
+                    <p>Đến ngày: <input type="text" id="datepicker2" class="form-control"></p>
+                </div>
+
+                <div class="col-md-2">
+                    <p>
+                        Lọc theo:
+                        <select class="dashboard-filter form-control">
+                            <option>--Chọn--</option>
+                            <option value="7ngay">7 ngày qua</option>
+                            <option value="thangtruoc">Tháng trước</option>
+                            <option value="thangnay">Tháng này</option>
+                            <option value="365ngayqua">365 ngày qua</option>
+                        </select>
+                    </p>
+                </div>
+            </form>
+
+            <div class="col-md-12">
+                <div id="chart" style="height: 250px"></div>
+            </div>
+        </div>
+
+        <div class="row">
+            <style type="text/css">
+                table.table.table-bordered.table-dark {
+                    background: #32383e;
+                }
+
+                table.table.table-bordered.table-dark tr th {
+                    color: #fff;
+                }
+
+            </style>
+            <p class="title-thongke">Thống kê truy cập</p>
+            <table class="table table-bordered table-dark">
+                <thead>
+                    <tr>
+                        <th>Đang online</th>
+                        <th>Tổng tháng trước</th>
+                        <th>Tổng tháng này</th>
+                        <th>Tổng một năm</th>
+                        <th>Tổng truy cập</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="col">{{ $visitor_count }}</td>
+                        <td scope="col">{{ $visitor_last_month_count }}</td>
+                        <td scope="col">{{ $visitor_this_month_count }}</td>
+                        <td scope="col">{{ $visitor_year_count }}</td>
+                        <td scope="col">{{ $visitors_total }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4 col-xs-12">
+                <p class="title-thongke">Thống kê tổng sản phẩm bài viết đơn hàng</p>
+                <div id="donut" class="morris-donut-inverse"></div>
+            </div>
+
+            <div class="col-md-4 col-xs-12">
+                <h3>Bài viết xem nhiều</h3>
+                <ol class="list_views">
+                    @foreach ($post_views as $key => $post)
+                        <li>
+                            <a href="{{ url('bai-viet/' . $post->post_slug) }}">{{ $post->post_title }} | <span
+                                style="color: #000">{{$post->post_views}}</span></a>
+                        </li>
+                    @endforeach
+                </ol>
+            </div>
+
+            <div class="col-md-4 col-xs-12">
+                <style type="text/css">
+                    ol.list_views {
+                        margin: 10px 0;
+                        color: #fff
+                    }
+                    ol.list_views a {
+                        color: orange;
+                        font-weight: 400
+                    }
+                </style>
+            </div>
+        </div>
+    </div>
 @endsection
