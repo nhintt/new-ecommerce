82734526c9409df55aec711f4c6a9ec8d6ff61a9
nhintt <nhintt2411@gmail.com>
2020-11-20 22:28:59 +0700

    Đổi UI email

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -318,57 +318,9 @@
             </div>
         </div>
 
-        <div class="footer-widget">
+        {{-- <div class="footer-widget">
             <div class="container">
                 <div class="row">
-                    <!-- <div class="col-sm-2">
-                        <div class="single-widget">
-                            <h2>Service</h2>
-                            <ul class="nav nav-pills nav-stacked">
-                                <li><a href="#">Online Help</a></li>
-                                <li><a href="#">Contact Us</a></li>
-                                <li><a href="#">Order Status</a></li>
-                                <li><a href="#">Change Location</a></li>
-                                <li><a href="#">FAQ’s</a></li>
-                            </ul>
-                        </div>
-                    </div> -->
-                    <!-- <div class="col-sm-2">
-                        <div class="single-widget">
-                            <h2>Quock Shop</h2>
-                            <ul class="nav nav-pills nav-stacked">
-                                <li><a href="#">T-Shirt</a></li>
-                                <li><a href="#">Mens</a></li>
-                                <li><a href="#">Womens</a></li>
-                                <li><a href="#">Gift Cards</a></li>
-                                <li><a href="#">Shoes</a></li>
-                            </ul>
-                        </div>
-                    </div> -->
-                    <!-- <div class="col-sm-2">
-                        <div class="single-widget">
-                            <h2>Policies</h2>
-                            <ul class="nav nav-pills nav-stacked">
-                                <li><a href="#">Terms of Use</a></li>
-                                <li><a href="#">Privecy Policy</a></li>
-                                <li><a href="#">Refund Policy</a></li>
-                                <li><a href="#">Billing System</a></li>
-                                <li><a href="#">Ticket System</a></li>
-                            </ul>
-                        </div>
-                    </div> -->
-                    <!-- <div class="col-sm-2">
-                        <div class="single-widget">
-                            <h2>About Shopper</h2>
-                            <ul class="nav nav-pills nav-stacked">
-                                <li><a href="#">Company Information</a></li>
-                                <li><a href="#">Careers</a></li>
-                                <li><a href="#">Store Location</a></li>
-                                <li><a href="#">Affillate Program</a></li>
-                                <li><a href="#">Copyright</a></li>
-                            </ul>
-                        </div>
-                    </div> -->
                     <div class="col-sm-3 col-sm-offset-1">
                         <div class="single-widget">
                             <h2>About Shopper</h2>
@@ -382,7 +334,7 @@
 
                 </div>
             </div>
-        </div>
+        </div> --}}
 
         <div class="footer-bottom">
             <div class="container">
@@ -392,6 +344,27 @@
                 </div>
             </div>
         </div>
+
+
+            <div class="sidebar-contact wrapper-contact-form contact-form">
+                <div class="toggle"></div>
+                <div class="footer-widget">
+                    <div class="container">
+                        <div class="row">
+                                <div class="single-widget">
+                                    <h2>About Shopper</h2>
+                                    <form action="#" class="searchform">
+                                        <input type="text" placeholder="Your email address" />
+                                        <button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
+                                        <p>Get the most recent updates from <br />our site and be updated your self...</p>
+                                    </form>
+                                </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
 
     </footer><!--/Footer-->
 
@@ -419,6 +392,15 @@
 
     <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v6.0&appId=2339123679735877&autoLogAppEvents=1"></script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.toggle').click(function() {
+            $('.sidebar-contact').toggleClass('active');
+            $('.toggle').toggleClass('active')
+        })
+    })
+</script>
 
 <!--Lọc giá sản phẩm-->
 <!-- <script type="text/javascript">
