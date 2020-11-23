7d78eee2085793585c165b889b2d7058a5ba8bb8
thuynt <thuybinh11997@gmail.com>
2020-11-23 17:46:20 +0700

    edit mailchimp

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -354,43 +354,45 @@
     <div class="toggle"></div>
 
     <!-- Begin Mailchimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-
-</style>
-<div id="mc_embed_signup">
-<form action="https://xyz.us7.list-manage.com/subscribe/post?u=42b033be5fd420caee311f779&amp;id=68201cfaff" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<h2>Đăng ký để nhận thông báo</h2>
-<div class="indicates-required"></div>
-<div class="mc-field-group">
-	<label for="mce-EMAIL">Email  <span class="asterisk">*</span>
-</label>
-	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-</div>
-<div class="mc-field-group">
-	<label for="mce-FNAME">Họ tên </label>
-	<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-</div>
-<div class="mc-field-group ">
-	<label for="mce-PHONE">Điện thoại </label>
-	<input type="text" name="PHONE" class="" value="" id="mce-PHONE">
-</div>
-
-	<div id="mce-responses" class="clear">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_42b033be5fd420caee311f779_68201cfaff" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Đăng ký" name="subscribe" id="mc-embedded-subscribe" class=" btn btn-warning"></div>
+    <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+    <style type="text/css">
+        #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+    </style>
+    <div id="mc_embed_signup">
+    <form action="https://xyz.us7.list-manage.com/subscribe/post?u=adf6e7f3c44e23904d10b6ab5&amp;id=6034d4fdd1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+        <h2>Đăng ký để nhận thông báo !</h2>
+    <div class="mc-field-group">
+        <label for="mce-EMAIL">Email </label>
+        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
     </div>
-</form>
-</div>
-
-
-
-<!--End mc_embed_signup-->
-
+    <div class="mc-field-group">
+        <label for="mce-NAME">Họ tên </label>
+        <input type="text" value="" name="NAME" class="" id="mce-NAME">
+    </div>
+    <div class="mc-field-group ">
+        <label for="mce-PHONE">Điện thoại </label>
+        <input type="text" name="PHONE" class="" value="" id="mce-PHONE">
+    </div>
+        <div id="mce-responses" class="clear">
+            <div class="response" id="mce-error-response" style="display:none"></div>
+            <div class="response" id="mce-success-response" style="display:none"></div>
+        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_adf6e7f3c44e23904d10b6ab5_6034d4fdd1" tabindex="-1" value=""></div>
+        <div class="clear"><input type="submit" value="Đăng ký" name="subscribe" id="mc-embedded-subscribe" style="background-color:#FE980F"class="button" ></div>
+        </div>
+    </form>
+    </div>
+    <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='NAME';ftypes[1]='text';fnames[4]='PHONE';ftypes[4]='phone'; /*
+     * Translated default messages for the $ validation plugin.
+     * Locale: VI (Vietnamese)
+     */
+    $.extend($.validator.messages, {
+     alert('cảm ơn đã đk')
+    });}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+    <!--End mc_embed_signup-->
 
 </div>
 
